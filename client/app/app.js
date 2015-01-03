ebdf0be69df545174ed84736e85adc382ba01f1d
sarahca <s.canadabc@gmail.com>
2015-01-02 17:06:27 -0800

    user service

--- client/app/app.js
+++ client/app/app.js
@@ -9,13 +9,62 @@ angular.module('lamusiqueApp', [
   'ui.bootstrap',
   'mediaPlayer'
 ])
-  .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
+  .config(function ($provide, $stateProvider, $urlRouterProvider, $locationProvider) {
     $urlRouterProvider
       .otherwise('/');
 
     $locationProvider.html5Mode(true);
-  })
+    $provide.factory("UserService", function() {
+      var user = {
+        points: 0,
+        gems: 0,
+        loggedIn: false,
+        admin: false,
+        username: 'New Player',
 
+        getUserUsername: function() {
+          return this.username;
+        },
+        getUserPoints: function() {
+          return this.points;
+        },
+        getUserGems: function() {
+          return this.gems;
+        },
+        getUserLoggedIn: function() {
+          return this.loggedIn;
+        },
+        getUserAdmin: function() {
+          return this.admin;
+        },
+        setUserUsername: function(username) {
+          this.username = username ;
+        },
+        setUserPoints: function(points) {
+          this.points = points;
+        },
+        setUserGems: function(gems) {
+          this.gems = gems;
+        },
+        setUserLoggedIn: function(loggedin){
+          this.loggedIn = loggedin;
+        },
+        setUserAdmin: function(admin) {
+          this.admin = admin;
+        },
+        saveUserDataOnLogin: function(data) {
+          this.setUserLoggedIn(true);
+          this.setUserUsername(data['user-username']);
+          this.setUserPoints(data['user-points']);
+          this.setUserGems(data['user-gems']);
+        },
+      }
+      return user;
+    });
+  })
   .service('MediaPlayer', function () {
     return angular.element(document.querySelector('audio')).scope().mediaPlayer;
-  })
+  });
+
+  
+
