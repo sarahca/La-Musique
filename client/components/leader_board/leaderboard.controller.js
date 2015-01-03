c4df15ed676ce717f8ced3938ff001ee1e7b4ee1
sarahca <s.canadabc@gmail.com>
2015-01-02 17:15:01 -0800

    navbar in progress

--- client/components/leader_board/leaderboard.controller.js
+++ client/components/leader_board/leaderboard.controller.js
@@ -2,4 +2,9 @@
 
 angular.module('lamusiqueApp')
   .controller('LeaderboardCtrl', function ($scope, $http, socket, $timeout, $rootScope) {
+
+    var leaders = [];
+    $rootScope.$on('refresh leaderboard', function (e, data) {
+      var newLeaders = data['leaders'];
+    });
   });
--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -1,9 +1,31 @@
 'use strict';
 
 angular.module('lamusiqueApp')
-  .controller('NavbarCtrl', function ($scope, $http) {
+  .controller('NavbarCtrl', function ($scope, $http, $timeout, $rootScope, UserService) {
+
+    $scope.user = UserService;
+    var controller = this;
+    controller.saveUserDataOnLogin = $scope.user.saveUserDataOnLogin;
+   
+    //check if user logged in
+    $timeout(function() {
+      $http.get('/api/user/isLoggedIn')
+        .success(function(data, status, headers, config) {
+          controller.saveUserDataOnLogin(data);
+        });
+    });
 
     $scope.logout = function() {
       $http.post('/api/user/logout');
+      $scope.user.setUserLoggedIn(false);
+    }
+
+    $scope.login = function() {
+      $scope.inLogin = true;
+    }
+
+    $scope.signup = function() {
+      $scope.inSignup = true;
     }
   });
+
