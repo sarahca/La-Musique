e21b4f238e99d636ac69d66ec505bc2513cbf89e
uiruson <uiruson@gmail.com>
2015-01-06 23:51:03 -0800

    Implemented new landing page, login and register form still not working

--- client/app/landing/jquery.pageslide.js
+++ client/app/landing/jquery.pageslide.js
@@ -1,208 +1,153 @@
 /*
- * jQuery pageSlide
- * Version 2.0
- * http://srobbin.com/jquery-pageslide/
- *
- * jQuery Javascript plugin which slides a webpage over to reveal an additional interaction pane.
- *
- * Copyright (c) 2011 Scott Robbin (srobbin.com)
- * Dual licensed under the MIT and GPL licenses.
+ * jQuery Panel Slider plugin v0.1.1
+ * https://github.com/eduardomb/jquery-panelslider
 */
+(function($) {
+  'use strict';
 
-;(function($){
-    // Convenience vars for accessing elements
-    var $body = $('body'),
-        $pageslide = $('#pageslide');
-    
-    var _sliding = false,   // Mutex to assist closing only once
-        _lastCaller;        // Used to keep track of last element to trigger pageslide
-    
-  // If the pageslide element doesn't exist, create it
-    if( $pageslide.length == 0 ) {
-         $pageslide = $('<div />').attr( 'id', 'pageslide' )
-                                  .css( 'display', 'none' )
-                                  .appendTo( $('body') );
+  var $body = $('body'),
+      _sliding = false;
+
+  function _slideIn(panel, options) {
+    var panelWidth = panel.outerWidth(true),
+        bodyAnimation = {},
+        panelAnimation = {};
+
+    if(panel.is(':visible') || _sliding) {
+      return;
     }
-    
-    /*
-     * Private methods 
-     */
-    function _load( url, useIframe ) {
-        // Are we loading an element from the page or a URL?
-        if ( url.indexOf("#") === 0 ) {                
-            // Load a page element                
-            $(url).clone(true).appendTo( $pageslide.empty() ).show();
-        } else {
-            // Load a URL. Into an iframe?
-            if( useIframe ) {
-                var iframe = $("<iframe />").attr({
-                                                src: url,
-                                                frameborder: 0,
-                                                hspace: 0
-                                            })
-                                            .css({
-                                                width: "100%",
-                                                height: "100%"
-                                            });
-                
-                $pageslide.html( iframe );
-            } else {
-                $pageslide.load( url );
-            }
-            
-            $pageslide.data( 'localEl', false );
-            
-        }
-    }
-    
-    // Function that controls opening of the pageslide
-    function _start( direction, speed ) {
-        var slideWidth = $pageslide.outerWidth( true ),
-            bodyAnimateIn = {},
-            slideAnimateIn = {};
-        
-        // If the slide is open or opening, just ignore the call
-        if( $pageslide.is(':visible') || _sliding ) return;         
-        _sliding = true;
-                                                                    
-        switch( direction ) {
-            case 'left':
-                $pageslide.css({ left: 'auto', right: '-' + slideWidth + 'px' });
-                bodyAnimateIn['margin-left'] = '-=' + slideWidth;
-                slideAnimateIn['right'] = '+=' + slideWidth;
-                break;
-            default:
-                $pageslide.css({ left: '-' + slideWidth + 'px', right: 'auto' });
-                bodyAnimateIn['margin-left'] = '+=' + slideWidth;
-                slideAnimateIn['left'] = '+=' + slideWidth;
-                break;
-        }
-                    
-        // Animate the slide, and attach this slide's settings to the element
-        $body.animate(bodyAnimateIn, speed);
-        $pageslide.show()
-                  .animate(slideAnimateIn, speed, function() {
-                      _sliding = false;
-                  });
 
-        
-    }
-      
-    /*
-     * Declaration 
-     */
-    $.fn.pageslide = function(options) {
-        var $elements = this;
-        
-        // On click
-        $elements.click( function(e) {
-            var $self = $(this),
-                settings = $.extend({ href: $self.attr('href') }, options);
-            
-            // Prevent the default behavior and stop propagation
-            e.preventDefault();
-            e.stopPropagation();
-            
-            if ( $pageslide.is(':visible') && $self[0] == _lastCaller ) {
-                // If we clicked the same element twice, toggle closed
-                $.pageslide.close();
-            } else {                 
-                // Open
-                $.pageslide( settings );
+    _sliding = true;
+    panel.addClass('ps-active-panel').css({
+      position: 'fixed',
+      top: 0,
+      height: '100%',
+      'z-index': 999999
+    });
+    panel.data(options);
 
-                // Record the last element to trigger pageslide
-                _lastCaller = $self[0];
-            }       
-        });                   
-  };
-  
-  /*
-     * Default settings 
-     */
-    $.fn.pageslide.defaults = {
-        speed:      200,        // Accepts standard jQuery effects speeds (i.e. fast, normal or milliseconds)
-        direction:  'right',    // Accepts 'left' or 'right'
-        modal:      false,      // If set to true, you must explicitly close pageslide using $.pageslide.close();
-        iframe:     true,       // By default, linked pages are loaded into an iframe. Set this to false if you don't want an iframe.
-        href:       null        // Override the source of the content. Optional in most cases, but required when opening pageslide programmatically.
-    };
-  
-  /*
-     * Public methods 
-     */
-  
-  // Open the pageslide
-  $.pageslide = function( options ) {     
-      // Extend the settings with those the user has provided
-        var settings = $.extend({}, $.fn.pageslide.defaults, options);
-      
-      // Are we trying to open in different direction?
-        if( $pageslide.is(':visible') && $pageslide.data( 'direction' ) != settings.direction) {
-
-            $.pageslide.close(function(){
-                _load( settings.href, settings.iframe );
-                _start( settings.direction, settings.speed );
-            });
-        } else {                
-            _load( settings.href, settings.iframe );
-            if( $pageslide.is(':hidden') ) {
-                _start( settings.direction, settings.speed );
-            }
-        }
-        
-        $pageslide.data( settings );
-        console.log("Opened");
-       
-  }
-  
-  // Close the pageslide
-  $.pageslide.close = function( callback ) {
-        var $pageslide = $('#pageslide'),
-            slideWidth = $pageslide.outerWidth( true ),
-            speed = $pageslide.data( 'speed' ),
-            bodyAnimateIn = {},
-            slideAnimateIn = {}
-                      
-        // If the slide isn't open, just ignore the call
-        if( $pageslide.is(':hidden') || _sliding ) return;          
-        _sliding = true;
-        
-        switch( $pageslide.data( 'direction' ) ) {
-            case 'left':
-                bodyAnimateIn['margin-left'] = '+=' + slideWidth;
-                slideAnimateIn['right'] = '-=' + slideWidth;
-                break;
-            default:
-                bodyAnimateIn['margin-left'] = '-=' + slideWidth;
-                slideAnimateIn['left'] = '-=' + slideWidth;
-                break;
-        }
-        
-        $pageslide.animate(slideAnimateIn, speed);
-        $body.animate(bodyAnimateIn, speed, function() {
-            $pageslide.hide();
-            _sliding = false;
-            if( typeof callback != 'undefined' ) callback();
+    switch (options.side) {
+      case 'left':
+        panel.css({
+          left: '-' + panelWidth + 'px',
+          right: 'auto'
         });
+        bodyAnimation['margin-left'] = '+=' + panelWidth;
+        panelAnimation.left = '+=' + panelWidth;
+        break;
 
+      case 'right':
+        panel.css({
+          left: 'auto',
+          right: '-' + panelWidth + 'px'
+        });
+        bodyAnimation['margin-left'] = '-=' + panelWidth;
+        panelAnimation.right = '+=' + panelWidth;
+        break;
     }
-  
-  /* Events */
-  
-  // Don't let clicks to the pageslide close the window
-    $pageslide.click(function(e) {
-        e.stopPropagation();
+
+    $body.animate(bodyAnimation, options.duration);
+    panel.show().animate(panelAnimation, options.duration, function() {
+      _sliding = false;
+
+      if(typeof options.onOpen == 'function') {
+        options.onOpen();
+      }
+    });
+  }
+
+  $.panelslider = function(element, options) {
+    var active = $('.ps-active-panel');
+    var defaults = {
+      side: 'left',     // panel side: left or right
+      duration: 400,    // Transition duration in miliseconds
+      clickClose: false, // If true closes panel when clicking outside it
+      onOpen: null      // When supplied, function is called after the panel opens
+    };
+
+    options = $.extend({}, defaults, options);
+
+    // If another panel is opened, close it before opening the new one
+    if(active.is(':visible') && active[0] != element[0]) {
+      $.panelslider.close(function() {
+        _slideIn(element, options);
+      });
+    } else if(!active.length || active.is(':hidden')) {
+      _slideIn(element, options);
+    }
+  };
+
+  $.panelslider.close = function(callback) {
+    var active = $('.ps-active-panel'),
+        duration = active.data('duration'),
+        panelWidth = active.outerWidth(true),
+        bodyAnimation = {},
+        panelAnimation = {};
+
+    if(!active.length || active.is(':hidden') || _sliding) {
+      return;
+    }
+
+    _sliding = true;
+
+    switch(active.data('side')) {
+      case 'left':
+        bodyAnimation['margin-left'] = '-=' + panelWidth;
+        panelAnimation.left = '-=' + panelWidth;
+        break;
+
+      case 'right':
+        bodyAnimation['margin-left'] = '+=' + panelWidth;
+        panelAnimation.right = '-=' + panelWidth;
+        break;
+    }
+
+    active.animate(panelAnimation, duration);
+    $body.animate(bodyAnimation, duration, function() {
+      active.hide();
+      active.removeClass('ps-active-panel');
+      _sliding = false;
+
+      if(callback) {
+        callback();
+      }
+    });
+  };
+
+  // Bind click outside panel and ESC key to close panel if clickClose is true
+  $(document).bind('click keyup', function(e) {
+    var active = $('.ps-active-panel');
+
+    if(e.type == 'keyup' && e.keyCode != 27) {
+      return;
+    }
+
+    if(active.is(':visible') && active.data('clickClose')) {
+      $.panelslider.close();
+    }
+  });
+
+  // Prevent click on panel to close it
+  $(document).on('click', '.ps-active-panel', function(e) {
+    e.stopPropagation();
+  });
+
+  $.fn.panelslider = function(options) {
+    this.click(function(e) {
+      var active = $('.ps-active-panel'),
+          panel = $(this.getAttribute('href'));
+
+      // Close panel if it is already opened otherwise open it
+      if (active.is(':visible') && panel[0] == active[0]) {
+        $.panelslider.close();
+      } else {
+        $.panelslider(panel, options);
+      }
+
+      e.preventDefault();
+      e.stopPropagation();
     });
 
-  // Close the pageslide if the document is clicked or the users presses the ESC key, unless the pageslide is modal
-  $(document).bind('click keyup', function(e) {
-      // If this is a keyup event, let's see if it's an ESC key
-        if( e.type == "keyup" && e.keyCode != 27) return;
-      
-      // Make sure it's visible, and we're not modal      
-      if( $pageslide.is( ':visible' ) && !$pageslide.data( 'modal' ) ) {          
-          $.pageslide.close();
-      }
-  });
-  
+    return this;
+  };
 })(jQuery);
--- client/app/landing/landing.controller.js
+++ client/app/landing/landing.controller.js
@@ -5,6 +5,29 @@ angular.module('lamusiqueApp')
   .controller('LandingCtrl', function ( $scope, $http, socket, $timeout, $rootScope, $location, UserService) {
   $scope.user = UserService;
 
+    $('#right-panel-link').panelslider({side: 'right', clickClose: false, duration: 700 });
+    $('#right-panel-link').on('click', function(){
+      $('.arrow-button').toggleClass("slideLeft");
+    });
+
+    //line 13-28 not responding, something to do with angular
+    $('#signUpLink').on('click', function(){
+      console.log("signUpLink clicked");
+      $('#login').hide();
+      $('#register').show();
+      $('#register-button').hide();
+      $('#backto-login-button').show();
+    });
+
+    $('#backToLogin').on('click', function(){
+      console.log("backToLogin clicked");
+      $('#login').show();
+      $('#register').hide();
+      $('#backto-login-button').hide();
+      $('#register-button').show();
+    });
+    
+
     var controller = this;
     controller.saveUserDataOnLogin = $scope.user.saveUserDataOnLogin;
     $rootScope.channel = (Math.floor(Math.random()*100000)).toString();
